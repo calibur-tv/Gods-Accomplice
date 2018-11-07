6c7475a1859b4b3001370cda37eb9d2248bd99f9
falstack <icesilt@outlook.com>
2018-11-07 23:23:20 +0800

    add: transcations

---
+++ src/containers/transactions/v1/app.vue
@@ -0,0 +1,34 @@
+<style lang="scss">
+p {
+  color: $color-pink;
+}
+</style>
+
+<template>
+  <div>
+    <p>hello world</p>
+    <img
+      src="@/images/logo.png"
+      width="300"
+      height="300"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {
+    console.log("hahhahahahahaa");
+  },
+  methods: {}
+};
+</script>
